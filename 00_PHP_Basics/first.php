18a7cb130e1bad4c63d8ba9b7f432f01470b7965
A-Al-Mamun <abdullahalmamun3073@gmail.com>
2024-10-03 18:55:24 +0600

    Create new Folder for Basic PHP Practice

---
+++ 00_PHP_Basics/first.php
@@ -0,0 +1,31 @@
+<?php
+    /* Structure 
+    of 
+    php 
+    practice */
+    echo "Abdullah Al Mamun"; //It's just first example for php learn basic with comment section
+    echo "<br>";
+
+    //Variable
+    $name = "Web Development";
+    echo $name . "<br><br>"; //Concatanation
+
+
+    //Data Type
+    /*  String 
+        Integer
+        Float
+        Boolean
+        Array
+        Object
+        Null
+    */
+    $test = "Hello World";
+    $test = 100;
+    $test = 100.30;
+    $test = false;
+    $test = array("HTML","CSS","JS");
+    $test = null;
+    var_dump( $test);
+    //var_export($test);
+?>
